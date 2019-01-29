2d41708fbb6f4f9afc152bcdc4ce10292c95c6db
Adam Gonda <adamgondagyula@gmail.com>
2019-01-29 10:19:37 +0100

    done

--- src/main/java/com/codecool/singletonDojo/Printer.java
+++ src/main/java/com/codecool/singletonDojo/Printer.java
@@ -1,14 +1,55 @@
 package com.codecool.singletonDojo;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Printer {
+
+    private static final int MAX_NUMBER_OF_PRINTERS = 10;
+    private static List<Printer> instances = new ArrayList<>();
+    private static int nextId = 0;
+    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ss");
 
     private int id; // ID of the printer. Unique.
     private LocalTime busyEndTime;
 
     public Printer(int id) {
         this.id = id;
+    }
+
+    public static Printer getInstance() {
+       if(instances.size() > 0){
+           // try find an available printer
+           for (Printer p : instances) {
+               if(p.isAvailable()){
+                   return p;
+               }
+           }
+           return getPrinter();
+
+       }else{
+           return getPrinter();
+       }
+    }
+
+    private static Printer getPrinter() {
+        if (instances.size() == MAX_NUMBER_OF_PRINTERS) {
+            //return a random printer
+            return getRandomPrinter();
+
+        } else {
+            // create new printer
+            Printer newPrinter = new Printer(nextId++);
+            instances.add(newPrinter);
+            return newPrinter;
+        }
+    }
+
+    private static Printer getRandomPrinter(){
+        return instances.get(new Random().nextInt(instances.size()));
     }
 
     // Prints out the given String
